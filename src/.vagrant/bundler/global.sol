18266a427ab01916849496b08710ae23068fc918
Mario Mora <mario.amsg@gmail.com>
2025-01-29 17:07:04 -0600

    Initial Commit from Host: src directory

---
+++ src/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["vagrant-vmware-desktop",["= 3.0.4"]]],"checksum":"ecc6a14f67b4e4cffe9b7ff42c612288d29be91090bacc6f3111b157a625ebb5","vagrant_version":"2.4.3"}
